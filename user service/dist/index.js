869da8b958e579307406674e4f37fe30c4a57024
Bhargav7322 <bhargav.meva@gmail.com>
2025-04-03 17:50:40 +0530

    Script Setup

---
+++ user service/dist/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+const connectDb = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URL, {
+            dbName: "Spotify",
+        });
+    }
+    catch (error) {
+    }
+};
+const port = process.env.PORT || 5000;
+const app = express();
+app.get("/", (req, res) => {
+    res.send("server is working");
+});
+app.listen(5000, () => {
+    console.log(`Server is running on port ${port}`);
+});
