112faea0557043c8a27f0756459c73476d065be2
Bhargav7322 <bhargav.meva@gmail.com>
2025-04-04 18:04:32 +0530

    Create User And All Setup

---
+++ user service/dist/controller.js
@@ -0,0 +1,28 @@
+import { User } from "./modal.js";
+import TryCatch from "./TryCatch.js";
+import bcrypt from "bcrypt";
+import Jwt from "jsonwebtoken";
+export const registerUser = TryCatch(async (req, res) => {
+    const { name, email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+        res.status(400).json({
+            message: "User already exists",
+        });
+        return;
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    user = await User.create({
+        name,
+        email,
+        password: hashPassword,
+    });
+    const token = Jwt.sign({ _id: user._id }, process.env.JWT_SEC, {
+        expiresIn: "7d",
+    });
+    res.status(201).json({
+        message: "User created successfully",
+        user,
+        token,
+    });
+});
--- user service/src/index.ts
+++ user service/src/index.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './route.js';
 
 dotenv.config()
-
 
 const connectDb = async () => {
 try {
     mongoose.connect(process.env.MONGO_URL as string,{
         dbName:"Spotify",
     })
+    console.log("Mongo Db Connectd")
 } catch (error) {
-    
+    console.log(error)
 }
 }
 
@@ -20,10 +21,15 @@ const port  = process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+
+app.use("/api/v1",userRoutes)
+
 app.get("/",(req,res)=>{
     res.send("server is working")
 })
 
 app.listen(5000, () => {
     console.log(`Server is running on port ${port}`)
+    connectDb()
 })
