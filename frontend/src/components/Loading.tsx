b662451cfea65abe72a8be3afbadd7fd5c06e885
Bhargav7322 <bhargav.meva@gmail.com>
2025-04-22 18:01:06 +0530

    Add Player

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+
+
+const Loading = () => {
+  return   <div className="flex justify-center items-center h-screen bg-[#121212]">
+        <div className="w-16 h-16 border-4 border-green-500 "></div>
+    </div>
+}
+
+export default Loading
--- frontend/src/context/SongContext.tsx
+++ frontend/src/context/SongContext.tsx
@@ -34,6 +34,10 @@ interface SongContextType {
   selectedSong: string | null;
   setSelectedSong: (id: string) => void;
   albums: Album[];
+  fetchSingleSong: () => Promise<void>;
+  song: Song | null;
+  nextSong: () => void;
+  prevSong: () => void;
 }
 
 const SongContext = createContext<SongContextType | undefined>(undefined);
@@ -43,17 +47,20 @@ interface SongProviderProps {
 }
 
 export const SongProvider: React.FC<SongProviderProps> = ({ children }) => {
-  const [songs, setSong] = useState<Song[]>([]);
+  const [songs, setSongs] = useState<Song[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [selectedSong, setSelectedSong] = useState<string | null>(null);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [albums, setAlbums] = useState<Album[]>([]);
+  const [song, setSong] = useState<Song | null>(null);
+  const [index, setIndex] = useState<number>(0);
+  
 
   const fetchSongs = useCallback(async () => {
     setLoading(true);
     try {
       const { data } = await axios.get<Song[]>(`${server}/api/v1/song/all`);
-      setSong(data);
+      setSongs(data);
       if (data.length > 0) {
         setSelectedSong(data[0].id.toString());
         setIsPlaying(false);
@@ -65,6 +72,18 @@ export const SongProvider: React.FC<SongProviderProps> = ({ children }) => {
     }
   }, []);
 
+  const fetchSingleSong = useCallback(async () => {
+    if (!selectedSong) return;
+    try {
+      const { data } = await axios.get<Song>(
+        `${server}/api/v1/song/${selectedSong}`
+      );
+      setSong(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [selectedSong]);
+
   const fetchAlbum = useCallback(async () => {
     try {
       const { data } = await axios.get<Album[]>(`${server}/api/v1/album/all`);
@@ -73,6 +92,23 @@ export const SongProvider: React.FC<SongProviderProps> = ({ children }) => {
       console.log(error);
     }
   }, []);
+
+ const nextSong = useCallback(() => {
+  if(index === songs.length - 1) {
+    setIndex(0)
+    setSelectedSong(songs[0]?.id.toString())
+  }else{
+    setIndex((prevIndex)=> prevIndex + 1)
+    setSelectedSong(songs[index + 1]?.id.toString())
+  }
+ },[index,songs])
+
+const prevSong = useCallback(() => {
+  if(index > 0) {
+    setIndex((prevIndex)=> prevIndex - 1)
+    setSelectedSong(songs[index - 1]?.id.toString())
+  }
+},[songs,index])
 
   useEffect(() => {
     fetchSongs();
@@ -88,7 +124,11 @@ export const SongProvider: React.FC<SongProviderProps> = ({ children }) => {
         setSelectedSong,
         isPlaying,
         loading,
-        albums
+        albums,
+        fetchSingleSong,
+        song,
+        nextSong,
+        prevSong,
       }}
     >
       {children}
